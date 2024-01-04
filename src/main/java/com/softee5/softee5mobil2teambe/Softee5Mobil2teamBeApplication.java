157a449bb661696d50a15109fce817836f8e5c62
insiderhj <insiderhj@gmail.com>
2024-01-04 13:33:15 +0900

    feat: initial commit

---
+++ src/main/java/com/softee5/softee5mobil2teambe/Softee5Mobil2teamBeApplication.java
@@ -0,0 +1,13 @@
+package com.softee5.softee5mobil2teambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Softee5Mobil2teamBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Softee5Mobil2teamBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softee5/softee5mobil2teambe/Softee5Mobil2teamBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softee5.softee5mobil2teambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Softee5Mobil2teamBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
