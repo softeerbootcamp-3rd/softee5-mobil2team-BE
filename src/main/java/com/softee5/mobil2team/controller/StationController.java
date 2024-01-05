689b3427fda07e1b1c9436023f7f36804724bfd3
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2024-01-06 01:05:26 +0900

    feat: 역 별 태그 리스트 조회

--- src/main/java/com/softee5/mobil2team/controller/StationController.java
+++ src/main/java/com/softee5/mobil2team/controller/StationController.java
@@ -1,9 +1,28 @@
 package com.softee5.mobil2team.controller;
 
+import com.softee5.mobil2team.dto.DataResponseDto;
+import com.softee5.mobil2team.dto.TagDto;
+import com.softee5.mobil2team.dto.TagListDto;
+import com.softee5.mobil2team.service.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/station")
 public class StationController {
+    private final TagService tagService;
+
+    @GetMapping("/tag/list")
+    public ResponseEntity<DataResponseDto<TagListDto>> getAllTags(@RequestParam Long id) {
+        return new ResponseEntity<>(tagService.getTagList(id), HttpStatus.OK);
+    }
 }
--- src/main/java/com/softee5/mobil2team/repository/PostRepository.java
+++ src/main/java/com/softee5/mobil2team/repository/PostRepository.java
@@ -4,6 +4,11 @@ import com.softee5.mobil2team.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    /* 해당 시간 이후 작성된 포스트 조회 */
+    List<Post> findAllByStationIdAndCreatedDatetimeAfter(Long id, Date createdDatetime);
 }
