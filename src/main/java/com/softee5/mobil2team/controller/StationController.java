0b5756a374d7bd54c39953a76055dda36cfd6977
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2024-01-06 01:24:11 +0900

    fix: resolve conflict

--- src/main/java/com/softee5/mobil2team/controller/StationController.java
+++ src/main/java/com/softee5/mobil2team/controller/StationController.java
@@ -1,9 +1,10 @@
 package com.softee5.mobil2team.controller;
 
-import com.softee5.mobil2team.dto.DataResponseDto;
-import com.softee5.mobil2team.dto.TagListDto;
+import com.softee5.mobil2team.dto.*;
+import com.softee5.mobil2team.service.StationService;
 import com.softee5.mobil2team.service.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +12,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.crypto.Data;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/station")
 public class StationController {
+
+    @Autowired
+    private StationService stationService;
     private final TagService tagService;
 
+    @GetMapping("/briefInfo")
+    public ResponseEntity<DataResponseDto<BriefInfoDto>> getTest() {
+        return new ResponseEntity<>(stationService.getBriefInfo(), HttpStatus.OK);
+    }
+
+    @GetMapping("/near")
+    public ResponseEntity<DataResponseDto<StationDto>> nearStation(
+            @RequestParam Double x,
+            @RequestParam Double y
+    ) {
+        if (x == null || y == null)
+            throw new GeneralException(ResponseCode.BAD_REQUEST);
+        return new ResponseEntity<>(stationService.nearestStation(x, y), HttpStatus.OK);
+    }
+
+    /* 가까운 역 리스트 */
+    @GetMapping("/near/list")
+    public ResponseEntity<DataResponseDto<StationListDto>> getNearStationList(
+            @RequestParam(value = "x", required = false) Double x,
+            @RequestParam(value = "y", required = false) Double y) {
+        return new ResponseEntity<>(
+                stationService.getNearStationList(x, y),
+                HttpStatus.OK
+        );
+    }
+
+    /* 역 별 태그 리스트 조회 */
     @GetMapping("/tag/list")
     public ResponseEntity<DataResponseDto<TagListDto>> getAllTags(@RequestParam Long id) {
         return new ResponseEntity<>(tagService.getTagList(id), HttpStatus.OK);
