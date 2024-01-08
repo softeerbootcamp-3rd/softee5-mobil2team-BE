a2d31f652051a36823444ce08956290986522a91
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2024-01-08 13:04:25 +0900

    feat: 태그 리스트 조회 예외처리

--- src/main/java/com/softee5/mobil2team/service/TagService.java
+++ src/main/java/com/softee5/mobil2team/service/TagService.java
@@ -1,11 +1,14 @@
 package com.softee5.mobil2team.service;
 
+import com.softee5.mobil2team.config.GeneralException;
+import com.softee5.mobil2team.config.ResponseCode;
 import com.softee5.mobil2team.dto.DataResponseDto;
 import com.softee5.mobil2team.dto.TagDto;
 import com.softee5.mobil2team.dto.TagListDto;
 import com.softee5.mobil2team.entity.Post;
 import com.softee5.mobil2team.entity.Tag;
 import com.softee5.mobil2team.repository.PostRepository;
+import com.softee5.mobil2team.repository.StationRepository;
 import com.softee5.mobil2team.repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,10 +21,17 @@ public class TagService {
     @Autowired
     private TagRepository tagRepository;
     @Autowired
-    PostRepository postRepository;
+    private PostRepository postRepository;
+    @Autowired
+    private StationRepository stationRepository;
 
     /* 역 별 태그 리스트 */
     public DataResponseDto<TagListDto> getTagList(Long id) {
+        // stationID 예외처리
+        if (stationRepository.findById(id).isEmpty()) {
+            throw new GeneralException(ResponseCode.BAD_REQUEST, "존재하지 않는 역 ID입니다.");
+        }
+
         // tag 목록 전체 조회
         List<Tag> tagList = tagRepository.findAll();
 
