2357a8ce76a42c3f220d8b185319f82ee2329aaa
insiderhj <insiderhj@gmail.com>
2024-01-08 12:39:17 +0900

    fix: cors 문제 해결

--- src/main/java/com/softee5/mobil2team/config/WebConfig.java
+++ src/main/java/com/softee5/mobil2team/config/WebConfig.java
@@ -5,12 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("http://localhost:5173", "https://mobil2team-fe.vercel.app");
+                .allowedOrigins("http://localhost:5173", "https://mobil2team-fe.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
