385a01774a0bc083095f9c506579b6cfcdfaba72
Sooah Han <s2sooas2@ewhain.net>
2024-01-08 17:07:09 +0900

    chore: change time zone

--- src/main/java/com/softee5/mobil2team/Mobil2teamApplication.java
+++ src/main/java/com/softee5/mobil2team/Mobil2teamApplication.java
@@ -1,13 +1,22 @@
 package com.softee5.mobil2team;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class Mobil2teamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Mobil2teamApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // timezone 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
